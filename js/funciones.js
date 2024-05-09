2047966d596608d967ba5543b8b3e7b5427f9c5b
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2024-05-09 20:38:13 -0300

    funciones promesas

--- js/funciones.js
+++ js/funciones.js
@@ -5,25 +5,76 @@
  4- si estan mal alerta
 */
 
-function isEmailValid ( emial ){
+const ERROR_COLOR = "red";
+const VALID_COLOR = "green";
+const DEFAULT_EMPTY = "";
+
+function isEmailValid(email) {
+    // Vamos a validar lo siguiente...    
+    // 1- ver que no arranque con arroba
+    // 2- Que tenga solo 1 arroba...
+    // 3- No puede ser la ultima letra la arroba...
+    // 4- deberia terminar con .com
+
+    let hasDotCom = email.endsWith(".com"); // Esto es el numero 4...si true entonces 3 es true...
+
+    let arrobaIndex = email.indexOf("@");
+    // if arrobaIndex === 0 OR arrobaInde === -1
+    let arrobaIncorrecto = (arrobaIndex === 0) || (arrobaIndex === -1)
+    if (arrobaIncorrecto) {
         return false;
+    }
+
+    // podria pasar que tuviera mas de 1 arroba... ale@@@@@mail.com
+    // Falta verificar...
+    let arrobaNoRepetido = true;
+
+
+    return hasDotCom && !arrobaIncorrecto && arrobaNoRepetido;
+
 }
 
-export function ValidadorFormulario( event ) {
-    
+function changeBorderColor(rgb, element) {
+    element.style.borderColor = rgb;
+}
+
+function conexionBackendAPI() {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            let fail = Math.random() > 0.5; // de prueba de forma aleatoria a veces es verdadero otras falso
+
+            if (fail) {
+                reject("Error: fallo irrecuperable");
+            } else {
+                resolve("Finalizacion satisfactoria");
+            }
+
+
+        }, 2000
+        )
+
+    });
+}
+
+
+export function ValidadorFormulario(event) {
+
+    event.preventDefault(); // a partir de esta linea el comportamiento del evento lo manejo YO - el Dev
+
     let miFormulario = event.currentTarget;
-    
+
     let isValid = true;
 
     for (let index = 0; index < miFormulario.length; index++) {
         const element = miFormulario[index];
-        
 
         switch (element.type) {
             case "text":
                 console.log("Encontre un campo text")
                 break;
-            
+
             case "checkbox":
                 break;
             case "email":
@@ -34,16 +85,30 @@ export function ValidadorFormulario( event ) {
                 break;
         }
 
-        if ( ! isValid) {
+        if (!isValid) {
             // podria emitir una alerta SOBRE el campo invalido
             // O cambiarle el color...
+            changeBorderColor(ERROR_COLOR, element);
             break; // el for se detiene...
+        } else {
+            changeBorderColor(VALID_COLOR, element);
         }
 
     }
 
-    if ( isValid){
+    if (isValid) {
         // envio TODO al backend
+
+        conexionBackendAPI()
+            .then(() => alert("salio bien"))
+            .catch( () => alert("salio mal"))
+            .finally( () => alert("Esta completo"));
+
+
+        /*setTimeout(() => {
+            alert("Data enviada correctamente al backend...")
+        }, 5000);// 5000 ms == 5 segundos..
+        */
     } else {
         //Mostrar cartel de que hay datos a corregir...
     }
